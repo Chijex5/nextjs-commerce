17cda7263d4a5dc6d7cdfd84c0f73ed7fad9b604
Jagaban <embroconnect2@outlook.com>
2026-01-10 19:07:09 +0100

    hello

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,25 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "prisma/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
+const databaseUrl = process.env.DATABASE_URL || process.env.DATABASE_URL;
+console.log("Database URL:", databaseUrl);
+if (!databaseUrl) {
+  throw new Error(
+    "No database URL found. Please set PRISMA_DATABASE_URL or DATABASE_URL in your .env"
+  );
+}
+
+const adapter = new PrismaPg({ databaseUrl });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    adapter,
   })
 
 if (process.env.NODE_ENV !== 'production') {
